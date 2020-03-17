9511a526e4b275fb361cb2d4652b89db5ec7fe96
Fenix <fenix4088@gmail.com>
2020-03-17 13:41:49 +0100

    Added plugins

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Filtration
+$('#portfolio-project-filter').mixItUp();
+
+//Nav page navigation
+$('#page-nav').onePageNav({
+    currentClass: 'active',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+    begin: function () {},
+    end: function () {},
+    scrollChange: function ($currentlistItem) {}
+});
