e554b033bd276bb59978a6dab48b8575287c8f85
Fenix <fenix4088@gmail.com>
2020-03-22 20:21:14 +0100

    Mobile adaptation and Mobile navigation finished

--- js/main.js
+++ js/main.js
@@ -1,15 +1,51 @@
-// Filtration
-$('#portfolio-project-filter').mixItUp();
 
-//Nav page navigation
-$('#page-nav').onePageNav({
-    currentClass: 'active',
-    changeHash: false,
-    scrollSpeed: 750,
-    scrollThreshold: 0.5,
-    filter: '',
-    easing: 'swing',
-    begin: function () {},
-    end: function () {},
-    scrollChange: function ($currentlistItem) {}
-});
+$(document).ready(function (){
+    // Filtration
+    $('#portfolio-project-filter').mixItUp();
+
+    //Nav page navigation
+    $('#page-nav').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function () {},
+        end: function () {},
+        scrollChange: function ($currentlistItem) {}
+    });
+
+    // click for Hamburger menue
+    const hamburgerBtn = document.querySelector('.header__burger');
+    const mobileMenue = document.querySelector('.header__navigation-block');
+    const bodyElement = document.querySelector('body');
+    const overlayElement = document.querySelector('.overlay');
+    const checkedInput = document.querySelector('.header__burger-checkbox');
+
+    hamburgerBtn.addEventListener('click', function() {
+        mobileMenue.classList.toggle('active');
+        bodyElement.classList.toggle('noscroll');
+        overlayElement.classList.toggle('active');
+    });
+
+    window.addEventListener('resize', function() {
+        mobileMenue.classList.remove('active');
+        bodyElement.classList.remove('noscroll');
+        overlayElement.classList.remove('active');
+        checkedInput.checked = false;
+    });
+
+    // Fixed navigation 
+    window.onscroll = function showNav() {
+        let navFixed = document.querySelector('.header__navigation');
+        
+
+        if(window.pageYOffset > 200) {
+            navFixed.classList.add('nav-fixed');
+        } else {
+            navFixed.classList.remove('nav-fixed');
+        }
+    }
+})
+
